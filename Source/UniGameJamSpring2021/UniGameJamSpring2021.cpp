17900b2cab30ebf2ec9f7a6d80f81c06a46492f1
Sam Croft <croft.sam77@gmail.com>
2021-03-26 18:41:32 +0000

    Initial commit

---
+++ Source/UniGameJamSpring2021/UniGameJamSpring2021.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UniGameJamSpring2021.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UniGameJamSpring2021, "UniGameJamSpring2021" );
---
+++ Source/UniGameJamSpring2021/UniGameJamSpring2021GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UniGameJamSpring2021GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNIGAMEJAMSPRING2021_API AUniGameJamSpring2021GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
